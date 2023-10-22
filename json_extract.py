4ad7cee7321bbff262a0dbe8b6091e7b8deb5802
GM <dragoonking2001@gmail.com>
2023-10-23 00:12:55 +0530

    Removed 2 files from stage

--- json_extract.py
+++ json_extract.py
@@ -2,9 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from fake_useragent import UserAgent
 import json
+import os
+import sys
 from webdriver_manager.chrome import ChromeDriverManager
-
+user_agent=UserAgent()
 
 def extract(url):
     '''Enter the URL for Json'''
@@ -15,11 +18,10 @@ def extract(url):
     chrome_options = Options()
     chrome_options.add_argument('--headless')  # Run Chrome in headless mode (no GUI)
     chrome_options.add_argument(
-        '--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36')  # Such that we behave like actual person
+        f'--user-agent={user_agent.random}')  # Such that we behave like actual person
 
     driver = webdriver.Chrome(service=service,
                               options=chrome_options)  # Now we can use 'driver' to interact with the web page
-
 
     driver.get(url)
 
@@ -36,44 +38,59 @@ def extract(url):
 
     return json_data
 
-def extract_all_url(city=12):
-    '''It extracts all the URL from the JSON of the site. Give the city number which you want to extract; check that in the site, you can find them in the network tab.'''
-    all_url = []
 
-    for page in range(1, 2589):
+def extract_all_url(city=12):
+    """It extracts all the URL from the JSON of the site. Give the city number which you want to extract; check that in the site, you can find them in the network tab."""
+    path_to_save_start=fr'{os.getcwd()}/Miscellaneous/start.txt'
+
+    # Create the directory if it does not exist
+    if not os.path.exists(path_to_save_start):
+        os.makedirs(path_to_save_start)
+
+    all_url = []
+    if os.path.exists(path_to_save_start):
+        with open(path_to_save_start, 'r') as f:
+            start_value = int(f.read())
+    else:
+        start_value = 1
+
+    for page in range(start_value, 2589):
         print(f'we are on page {page}')
         json_url = f"https://www.99acres.com/api-aggregator/discovery/srp/search?area_unit=1&platform=DESKTOP&moduleName=GRAILS_SRP&workflow=GRAILS_SRP&page_size=25&page={page}&city={city}&preference=S&res_com=R&seoUrlType=DEFAULT&recomGroupType=VSP&pageName=SRP&groupByConfigurations=true&lazy=true"
         a = extract(json_url)
 
-    try:
-        for i in range(len(a['properties'])):
-            try:
-                b = 'https://www.99acres.com/' + a['properties'][i]['PROP_DETAILS_URL']
-                all_url.append(b)
-            except KeyError:
+        try:
+            for properties_urls in range(len(a['properties'])):
                 try:
-                    b = a['properties'][i]['landingPage']['url']
+                    b = 'https://www.99acres.com/' + a['properties'][properties_urls]['PROP_DETAILS_URL']
                     all_url.append(b)
                 except KeyError:
-                    # Handle the situation where 'landingPage' key or 'url' key does not exist
-                    # You can raise a custom exception, log a message, or perform any other appropriate action.
-                    print("Error: 'landingPage' or 'url' key not found.")
-                    continue
-    except Exception as e:
-        print(f"Error occurred: {e}")
-    # Handle other exceptions here if needed
+                    try:
+                        b = a['properties'][properties_urls]['landingPage']['url']
+                        all_url.append(b)
+                    except KeyError:
+                        # Handle the situation where 'landingPage' key or 'url' key does not exist
+                        # You can raise a custom exception, log a message, or perform any other appropriate action.
+                        print("Error: 'landingPage' or 'url' key not found.")
 
+        except Exception as e:
+            print(f"Error occurred: {e}")
+            with open('all_url.txt', 'a') as f:
+                for m in all_url:
+                    f.write(m + '\n')
+            with open(path_to_save_start, 'w') as s:
+                s.write(str(page))
 
-    print("Extraction completed.")
+            sys.exit()
+
+        # Handle other exceptions here if needed
+
+        print("Extraction completed.")
     return all_url
 
 
-
-
-
-if __name__=='__main__':
-    e=extract_all_url()
-    with open('all_url.txt','w') as file:
+if __name__ == '__main__':
+    e = extract_all_url()
+    with open('all_url2.txt', 'a') as file:
         for i in e:
-            file.write(i+'\n')
-
+            file.write(i + '\n')
